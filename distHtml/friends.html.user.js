80b39ab528edb591ce98deb304d765b64e7db404
Zirolio <25364zirolio@gmail.com>
2024-03-22 16:49:26 +0200

    test

--- distHtml/friends.html.user.js
+++ distHtml/friends.html.user.js
@@ -1,6 +1,6 @@
 <!-- <link rel="stylesheet" href="./main.css"> -->
 <body class="bg-[#222]">
-    <button class="float-left border left-1/2 absolute translate-x-[-500px] rounded-t-[20px] border-[#585858] text-[#ffebc0] px-2 select-none hover:bg-[rgba(255,255,255,0.15)]">Changelog</button>
+    <button class="float-left border left-1/2 absolute translate-x-[-500px] rounded-t-[20px] border-[#585858] text-[#ffebc0] px-2 select-none hover:bg-[rgba(255,255,255,0.15)]" id="ChangeChlFr">Friends</button>
     <div class="float-left w-[300px] h-[250px] text-[18px] border-[1px] border-solid border-[#585858] rounded-[5px] rounded-tl-none relative left-1/2 overflow-auto translate-x-[-500px] translate-y-[25px] pathTrans" id="friendsList">
         <div class="text-[#ffebc0]" id="online">
             <div class="border-b w-full text-center py-1 text-[20px] mb-2 text-green-500 border-green-500 rounded select-none">Online</div>
