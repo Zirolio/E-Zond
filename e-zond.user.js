2ff2ef66da11ecba20fc7fd02d4c3cd237e78e09
Zirolio <25364zirolio@gmail.com>
2024-03-28 22:33:38 +0200

    -V10

--- e-zond.user.js
+++ e-zond.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name           E-Zond-Beta-d
-// @name:ru        E-Zond-Beta-d
+// @name           E-Zond-Beta
+// @name:ru        E-Zond-Beta
 // @namespace      http://tampermonkey.net/
 // @version        10
 // @description    Script for evades.io
