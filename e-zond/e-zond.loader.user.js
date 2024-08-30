a0a8f59368ca7f2c35de1b08f766bf4507e1fa76
Zirolio <25364Zirolio@gmail.com>
2024-08-30 22:41:17 +0300

    Update

--- e-zond/e-zond.loader.user.js
+++ e-zond/e-zond.loader.user.js
@@ -26,5 +26,5 @@
 //                                                             ^--------------------------------------------------------------------------------------------------------------------^
 
 (async() => {
-    eval(localStorage.getItem("e-zond-code") || (await fetch(`https://raw.githubusercontent.com/Zirolio/E-Zond/main/e-zond/e-zond.user.js`)).text());
+    eval(localStorage.getItem("e-zond-code") || await fetch(`https://raw.githubusercontent.com/Zirolio/E-Zond/main/e-zond/e-zond.user.js`).then(d => d.text()));
 })();
